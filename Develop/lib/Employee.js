ce5bc3b17df26ebcc4d5ab3fe173c7e85f718169
nrmarsh36 <nrmarsh36@gmail.com>
2020-08-10 15:17:20 -0500

    removed if false statements in employee constructor. apparently the tests don't like that. also fixed getRole model

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,18 +1,6 @@
 // TODO: Write code to define and export the Employee class
 class Employee {
     constructor(name, id, email, role) {
-        if (!name) {
-            throw new Error("You must enter a name.");
-        }
-        if (!id) {
-            throw new Error("You must enter your ID number.");
-        }
-        if (!email) {
-            throw new Error("You must enter an email address.");
-        }
-        if (!role) {
-            return ("Employee")
-        };
         this.name = name;
         this.id = id;
         this.email = email;
@@ -31,12 +19,12 @@ class Employee {
 
     getEmail() {
         console.log(`Name: ${this.email}`);
-        return ("Employee");
+        return this.email;
     };
 
     getRole() {
         console.log(`Name: ${this.role}`);
-        return this.role;
+        return ("Employee");
     };
 };
 
