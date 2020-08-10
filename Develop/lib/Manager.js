a28720d5f4a692c6f98446b35fcb2c57a4575a23
nrmarsh36 <nrmarsh36@gmail.com>
2020-08-10 15:07:54 -0500

    added manager class

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee");
+
+
+// Do I need ot add role into the constructor parameters???
+class Engineer extends Employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+    };
+    getOfficeNumber() {
+        console.log(`Office Number: ${this.officeNumber}`);
+        return this.officeNumber;
+    };
+    getRole() {
+        console.log(`Role: Manager`);
+        return ("Manager");
+    };
+};
+
+module.exports = Manager
